997c88c788e37888c479fe41effc8e8bacdc3c3a
keith <keith.perkins@cnu.edu>
2020-09-29 21:39:34 -0400

    added some output

--- src/4_vector_sort.cpp
+++ src/4_vector_sort.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 //dont have to write std:: in front of everything
 using std::cin;
 using std::cout;
@@ -22,6 +21,7 @@ using std::sort;
 using std::streamsize;
 using std::string;
 using std::vector;
+const int RES_SIZE=20;
 
 struct Student_info {
 	string name;
@@ -37,18 +37,29 @@ bool compareName(const Student_info& x, const Student_info& y)
 }
 bool compareMidterm(const Student_info& x, const Student_info& y)
 {
-	return x.midterm < y.midterm;
+	return x.midterm > y.midterm;
 }
 bool compareFinal(const Student_info& x, const Student_info& y)
 {
-	return x.final < y.final;
+	return x.final > y.final;
 }
+
+void output_contents(const string &phrase, const vector<Student_info> &students) {
+	cout<<phrase<<endl;
+	for (int var = 0; var < students.size(); ++var) {
+		cout<<"  "<<students[var].name<<", "<<students[var].midterm<<","<<students[var].final<<endl;
+	}
+}
+
 int main() {
 
 	//students holds a collection of
 	//student_info objects
 	vector<Student_info> students;
+	students.reserve(RES_SIZE);
 	int size = students.size();
+	int cap = students.capacity();
+	bool bemp = students.empty();
 
 	//just need 1 since vector will make a copy when adding it
 	Student_info record;
@@ -57,6 +68,7 @@ int main() {
 	record.midterm = 50;
 	record.final = 100;
 	students.push_back(record);	//add record, vector makes a COPY of it
+	cap = students.capacity();
 
 	record.name = "Keith";		//so we can reuse it
 	record.midterm = 100;
@@ -67,8 +79,12 @@ int main() {
 	record.midterm = 98;
 	record.final = 44;
 	students.push_back(record);
+	cap = students.capacity();
 
 	size = students.size();
+
+	//name of first entry in vector
+	string name1 = students[0].name;
 
 	//-------------------------------------
 	//simple sorting
@@ -93,18 +109,15 @@ int main() {
 
 	//by name
 	sort(students.begin(), students.end(), compareName);
+	output_contents(string("Sorted by Name "),students);
 
-	for (int var = 0; var <=size-1; ++var) {
-		name = students[var].name;
-	}
 	//sort by Midterm
 	sort(students.begin(), students.end(), compareMidterm);
-	for (int var = 0; var <=size-1; ++var) {
-		midterm = students[var].midterm;
-	}
+	output_contents(string("Sorted by Midterm "),students);
 
 	//sort by Final
 	sort(students.begin(), students.end(), compareFinal);
+	output_contents(string("Sorted by Final "),students);
 
 
 return 0;
